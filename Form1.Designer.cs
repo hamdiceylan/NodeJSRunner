0378883971e6e2c4ce2ee65d91a2f3e184e928eb
IbrahimErsoy <info@iersoy.com>
2014-02-26 10:04:22 +0200

    Uploaded The Application

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace NodeJSRunner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nodeJSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.getJSFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nodeJSToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(442, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nodeJSToolStripMenuItem
+            // 
+            this.nodeJSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.getJSFileToolStripMenuItem});
+            this.nodeJSToolStripMenuItem.Name = "nodeJSToolStripMenuItem";
+            this.nodeJSToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.nodeJSToolStripMenuItem.Text = "Node.JS";
+            // 
+            // getJSFileToolStripMenuItem
+            // 
+            this.getJSFileToolStripMenuItem.Name = "getJSFileToolStripMenuItem";
+            this.getJSFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.getJSFileToolStripMenuItem.Text = "Assign JS...";
+            this.getJSFileToolStripMenuItem.Click += new System.EventHandler(this.getJSFileToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nodeJSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem getJSFileToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NodeJSRunner
+{
+    public partial class Form1 : Form
+    {
+        string path = @"C:\Program Files\nodejs\nodevars.bat";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getJSFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            openFileDialog1.ShowDialog();
+            string[] lines = File.ReadAllLines(openFileDialog1.FileName);
+            foreach (string line in lines)
+            {
+                if (line.Contains("listen"))
+                {
+                    var regex = Regex.Match(line, @"\d+");
+                    if (regex.ToString() != "")
+                    {
+                        result = regex.ToString();
+                    }
+                }
+            }
+
+            string jsfile = openFileDialog1.FileName;
+            string query = "start /b /min node ";
+            string cmd = "start /b /min node " + jsfile;
+            string[] sutunlar = File.ReadAllLines(path);
+            foreach (string item in sutunlar)
+            {
+                if (item.StartsWith(query))
+                {
+                    break;
+                }
+
+                else
+                {
+                    StreamWriter sw = new StreamWriter(path, true);
+                    sw.WriteLine(Environment.NewLine);
+                    sw.WriteLine(cmd);
+                    sw.Close();
+                    sw.Dispose();
+                    break;
+                }
+            }
+
+            string fullPath = @"cmd.exe";
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = fullPath;
+            psi.WorkingDirectory = @"C:\Program Files\nodejs";
+            psi.Arguments = "/k nodevars.bat";
+            Process.Start(psi);
+            string urlpath = "http://localhost:" + result + "/";
+            Process.Start(urlpath);           
+        }
+
+        //Miguel's code from StackOverFlow: http://stackoverflow.com/questions/4264117/c-sharp-how-to-delete-last-line-in-a-text-file
+        public void DeleteLastLine(string filepath)
+        {
+            List<string> lines = File.ReadAllLines(filepath).ToList();
+            File.WriteAllLines(filepath, lines.GetRange(0, lines.Count - 1));
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            DeleteLastLine(path);
+        }
+    }
+}
